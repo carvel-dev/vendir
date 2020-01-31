cbb3f93b44a3b5e8b55d1ef50c0638358f3ca863
Dmitriy Kalinin <cppforlife@gmail.com>
2020-01-31 10:35:11 -0500

    fix version cmd e2e test

--- test/e2e/version_test.go
+++ test/e2e/version_test.go
@@ -9,7 +9,7 @@ func TestVersion(t *testing.T) {
 	env := BuildEnv(t)
 	out := Vendir{t, env.BinaryPath, Logger{}}.Run([]string{"version"})
 
-	if !strings.Contains(out, "Client Version") {
+	if !strings.Contains(out, "vendir version") {
 		t.Fatalf("Expected to find client version")
 	}
 }
