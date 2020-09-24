5e890fb0125e4bd38176eabc3f28a563e545386e
Dmitriy Kalinin <dkalinin@vmware.com>
2020-09-24 13:30:26 -0400

    consolidate file filtering

--- pkg/vendir/directory/directory.go
+++ pkg/vendir/directory/directory.go
@@ -50,6 +50,9 @@ func (d *Directory) Sync(syncOpts SyncOpts) (ctlconf.LockDirectory, error) {
 			return lockConfig, err
 		}
 
+		lockDirContents := ctlconf.LockDirectoryContents{Path: contents.Path}
+		skipFileFilter := false
+
 		switch {
 		case contents.Git != nil:
 			d.ui.PrintLinef("%s + %s (git from %s@%s)",
@@ -57,56 +60,32 @@ func (d *Directory) Sync(syncOpts SyncOpts) (ctlconf.LockDirectory, error) {
 
 			gitSync := ctlgit.NewSync(*contents.Git, NewInfoLog(d.ui), syncOpts.RefFetcher)
 
-			gitLockConf, err := gitSync.Sync(stagingDstPath, stagingDir.TempArea())
+			lock, err := gitSync.Sync(stagingDstPath, stagingDir.TempArea())
 			if err != nil {
 				return lockConfig, fmt.Errorf("Syncing directory '%s' with git contents: %s", contents.Path, err)
 			}
 
-			err = FileFilter{contents}.Apply(stagingDstPath)
-			if err != nil {
-				return lockConfig, fmt.Errorf("Filtering paths in directory '%s': %s", contents.Path, err)
-			}
-
-			lockConfig.Contents = append(lockConfig.Contents, ctlconf.LockDirectoryContents{
-				Path: contents.Path,
-				Git:  &gitLockConf,
-			})
+			lockDirContents.Git = &lock
 
 		case contents.HTTP != nil:
 			d.ui.PrintLinef("%s + %s (http from %s)", d.opts.Path, contents.Path, contents.HTTP.URL)
 
-			httpLockConf, err := ctlhttp.NewSync(*contents.HTTP, syncOpts.RefFetcher).Sync(stagingDstPath, stagingDir.TempArea())
+			lock, err := ctlhttp.NewSync(*contents.HTTP, syncOpts.RefFetcher).Sync(stagingDstPath, stagingDir.TempArea())
 			if err != nil {
 				return lockConfig, fmt.Errorf("Syncing directory '%s' with HTTP contents: %s", contents.Path, err)
 			}
 
-			err = FileFilter{contents}.Apply(stagingDstPath)
-			if err != nil {
-				return lockConfig, fmt.Errorf("Filtering paths in directory '%s': %s", contents.Path, err)
-			}
-
-			lockConfig.Contents = append(lockConfig.Contents, ctlconf.LockDirectoryContents{
-				Path: contents.Path,
-				HTTP: &httpLockConf,
-			})
+			lockDirContents.HTTP = &lock
 
 		case contents.Image != nil:
 			d.ui.PrintLinef("%s + %s (image from %s)", d.opts.Path, contents.Path, contents.Image.URL)
 
-			imageLockConf, err := ctlimg.NewSync(*contents.Image, syncOpts.RefFetcher).Sync(stagingDstPath)
+			lock, err := ctlimg.NewSync(*contents.Image, syncOpts.RefFetcher).Sync(stagingDstPath)
 			if err != nil {
 				return lockConfig, fmt.Errorf("Syncing directory '%s' with image contents: %s", contents.Path, err)
 			}
 
-			err = FileFilter{contents}.Apply(stagingDstPath)
-			if err != nil {
-				return lockConfig, fmt.Errorf("Filtering paths in directory '%s': %s", contents.Path, err)
-			}
-
-			lockConfig.Contents = append(lockConfig.Contents, ctlconf.LockDirectoryContents{
-				Path:  contents.Path,
-				Image: &imageLockConf,
-			})
+			lockDirContents.Image = &lock
 
 		case contents.GithubRelease != nil:
 			sync := ctlghr.NewSync(*contents.GithubRelease, syncOpts.GithubAPIToken, syncOpts.RefFetcher)
@@ -114,20 +93,12 @@ func (d *Directory) Sync(syncOpts SyncOpts) (ctlconf.LockDirectory, error) {
 			desc, _, _ := sync.DescAndURL()
 			d.ui.PrintLinef("%s + %s (github release %s)", d.opts.Path, contents.Path, desc)
 
-			lockConf, err := sync.Sync(stagingDstPath, stagingDir.TempArea())
+			lock, err := sync.Sync(stagingDstPath, stagingDir.TempArea())
 			if err != nil {
 				return lockConfig, fmt.Errorf("Syncing directory '%s' with github release contents: %s", contents.Path, err)
 			}
 
-			err = FileFilter{contents}.Apply(stagingDstPath)
-			if err != nil {
-				return lockConfig, fmt.Errorf("Filtering paths in directory '%s': %s", contents.Path, err)
-			}
-
-			lockConfig.Contents = append(lockConfig.Contents, ctlconf.LockDirectoryContents{
-				Path:          contents.Path,
-				GithubRelease: &lockConf,
-			})
+			lockDirContents.GithubRelease = &lock
 
 		case contents.HelmChart != nil:
 			helmChartSync := ctlhelmc.NewSync(*contents.HelmChart, syncOpts.HelmBinary, syncOpts.RefFetcher)
@@ -135,20 +106,12 @@ func (d *Directory) Sync(syncOpts SyncOpts) (ctlconf.LockDirectory, error) {
 			d.ui.PrintLinef("%s + %s (helm chart from %s)",
 				d.opts.Path, contents.Path, helmChartSync.Desc())
 
-			chartLockConf, err := helmChartSync.Sync(stagingDstPath, stagingDir.TempArea())
+			lock, err := helmChartSync.Sync(stagingDstPath, stagingDir.TempArea())
 			if err != nil {
 				return lockConfig, fmt.Errorf("Syncing directory '%s' with helm chart contents: %s", contents.Path, err)
 			}
 
-			err = FileFilter{contents}.Apply(stagingDstPath)
-			if err != nil {
-				return lockConfig, fmt.Errorf("Filtering paths in directory '%s': %s", contents.Path, err)
-			}
-
-			lockConfig.Contents = append(lockConfig.Contents, ctlconf.LockDirectoryContents{
-				Path:      contents.Path,
-				HelmChart: &chartLockConf,
-			})
+			lockDirContents.HelmChart = &lock
 
 		case contents.Manual != nil:
 			d.ui.PrintLinef("%s + %s (manual)", d.opts.Path, contents.Path)
@@ -160,10 +123,8 @@ func (d *Directory) Sync(syncOpts SyncOpts) (ctlconf.LockDirectory, error) {
 				return lockConfig, fmt.Errorf("Moving directory '%s' to staging dir: %s", srcPath, err)
 			}
 
-			lockConfig.Contents = append(lockConfig.Contents, ctlconf.LockDirectoryContents{
-				Path:   contents.Path,
-				Manual: &ctlconf.LockDirectoryContentsManual{},
-			})
+			lockDirContents.Manual = &ctlconf.LockDirectoryContentsManual{}
+			skipFileFilter = true
 
 		case contents.Directory != nil:
 			d.ui.PrintLinef("%s + %s (directory)", d.opts.Path, contents.Path)
@@ -173,37 +134,30 @@ func (d *Directory) Sync(syncOpts SyncOpts) (ctlconf.LockDirectory, error) {
 				return lockConfig, fmt.Errorf("Copying another directory contents into directory '%s': %s", contents.Path, err)
 			}
 
-			err = FileFilter{contents}.Apply(stagingDstPath)
-			if err != nil {
-				return lockConfig, fmt.Errorf("Filtering paths in directory '%s': %s", contents.Path, err)
-			}
-
-			lockConfig.Contents = append(lockConfig.Contents, ctlconf.LockDirectoryContents{
-				Path:      contents.Path,
-				Directory: &ctlconf.LockDirectoryContentsDirectory{},
-			})
+			lockDirContents.Directory = &ctlconf.LockDirectoryContentsDirectory{}
 
 		case contents.Inline != nil:
 			d.ui.PrintLinef("%s + %s (inline)", d.opts.Path, contents.Path)
 
-			inlineLockConf, err := ctlinl.NewSync(*contents.Inline, syncOpts.RefFetcher).Sync(stagingDstPath)
+			lock, err := ctlinl.NewSync(*contents.Inline, syncOpts.RefFetcher).Sync(stagingDstPath)
 			if err != nil {
 				return lockConfig, fmt.Errorf("Syncing directory '%s' with inline contents: %s", contents.Path, err)
 			}
 
-			err = FileFilter{contents}.Apply(stagingDstPath)
-			if err != nil {
-				return lockConfig, fmt.Errorf("Filtering paths in directory '%s': %s", contents.Path, err)
-			}
-
-			lockConfig.Contents = append(lockConfig.Contents, ctlconf.LockDirectoryContents{
-				Path:   contents.Path,
-				Inline: &inlineLockConf,
-			})
+			lockDirContents.Inline = &lock
 
 		default:
 			return lockConfig, fmt.Errorf("Unknown contents type for directory '%s'", contents.Path)
 		}
+
+		if !skipFileFilter {
+			err = FileFilter{contents}.Apply(stagingDstPath)
+			if err != nil {
+				return lockConfig, fmt.Errorf("Filtering paths in directory '%s': %s", contents.Path, err)
+			}
+		}
+
+		lockConfig.Contents = append(lockConfig.Contents, lockDirContents)
 	}
 
 	err = stagingDir.Replace(d.opts.Path)
