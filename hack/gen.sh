7a076ecfdfcfc6c2cac23e1b439829da1727369f
Dmitriy Kalinin <dkalinin@vmware.com>
2021-08-21 19:44:51 -0400

    simplify ./hack/gen.sh

--- hack/gen.sh
+++ hack/gen.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-set -e
+set -e -x
 
 # Note if you are not seeing generated code, most likely it's being placed into a different folder
 # (e.g. Do you have GOPATH directory structure correctly named for this project?)
 
-rm -rf pkg/client
-
-gen_groups_path=./vendor/k8s.io/code-generator/generate-groups.sh
-
-chmod +x $gen_groups_path
-
-$gen_groups_path \
-	deepcopy ""  github.com/vmware-tanzu/carvel-vendir/pkg/vendir versions:v1alpha1 \
+# Based on vendor/k8s.io/code-generator/generate-groups.sh
+go run vendor/k8s.io/code-generator/cmd/deepcopy-gen/main.go \
+	--input-dirs github.com/vmware-tanzu/carvel-vendir/pkg/vendir/versions/v1alpha1 \
+	-O zz_generated.deepcopy \
+	--bounding-dirs github.com/vmware-tanzu/carvel-vendir/pkg/vendir \
 	--go-header-file ./hack/gen-boilerplate.txt
 
-chmod -x $gen_groups_path
-
+# To keep things simple for now, vendir is not including protobuf files.
+# Projects (e.g. kapp-controller) including vendir should generate it
+# for pkg/vendir/versions/v1alpha1 directory.
