6a35725e8dfa7e77f50e4d207b97f49dbf929e6c
Daniel Helfand <helfand.4@gmail.com>
2021-09-17 14:03:31 -0500

    automate release process for vendir
    Also adds darwin arm64 binary

--- hack/build.sh
+++ hack/build.sh
@@ -2,16 +2,21 @@
 
 set -e -x -u
 
+function get_latest_git_tag {
+  git describe --tags | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+'
+}
+
+VERSION="${1:-`get_latest_git_tag`}"
+
 # makes builds reproducible
 export CGO_ENABLED=0
-repro_flags="-ldflags=-buildid= -trimpath -mod=vendor"
+LDFLAGS="-X github.com/vmware-tanzu/carvel-vendir/pkg/vendir/version.Version=$VERSION -buildid="
 
 go mod vendor
 go mod tidy
 go fmt ./cmd/... ./pkg/... ./test/...
 
-# export GOOS=linux GOARCH=amd64
-go build $repro_flags -o vendir ./cmd/vendir/...
+go build -ldflags="$LDFLAGS" -trimpath -mod=vendor -o vendir ./cmd/vendir/...
 ./vendir version
 
 # compile tests, but do not run them: https://github.com/golang/go/issues/15513#issuecomment-839126426
