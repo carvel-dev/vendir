0b6d143dd85ed2e838d04a98522496a2c1b4c3d1
Joao Pereira <joaod@vmware.com>
2022-08-22 14:35:29 -0500

    Errors out when changes are made to vdemeester library
    Due to the inclusion of imgpkg as a library we need to ensure that the
    change made in https://github.com/vmware-tanzu/carvel-imgpkg/pull/255
    stays in effect.
    This will behavior can be removed when we complete the issue
    https://github.com/vmware-tanzu/carvel-imgpkg/issues/397
    
    Signed-off-by: Joao Pereira <joaod@vmware.com>

--- hack/build.sh
+++ hack/build.sh
@@ -11,6 +11,18 @@ repro_flags="-trimpath -mod=vendor"
 
 go mod vendor
 go mod tidy
+
+# related to https://github.com/vmware-tanzu/carvel-imgpkg/pull/255
+# there doesn't appear to be a simple way to disable the defaultDockerConfigProvider
+# Having defaultDockerConfigProvider enabled by default results in the imgpkg auth ordering not working correctly
+# Specifically, the docker config.json is loaded before cli flags (and maybe even IaaS metadata services)
+git apply --ignore-space-change --ignore-whitespace ./hack/patch-k8s-pkg-credentialprovider.patch
+
+git diff --exit-code vendor/github.com/vdemeester || {
+  echo 'found changes in the project. when expected none. exiting'
+  exit 1
+}
+
 go fmt ./cmd/... ./pkg/... ./test/...
 
 # export GOOS=linux GOARCH=amd64
