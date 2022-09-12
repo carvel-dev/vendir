683a4bdcf2fec13b45f10d1d10f5ddd2dfdeac53
Joao Pereira <joaod@vmware.com>
2022-09-12 15:23:38 -0500

    Revert "Errors out when changes are made to vdemeester library"
    With imgpkg 0.32.0 there is not need for this hack script anymore
    
    This reverts commit 0b6d143dd85ed2e838d04a98522496a2c1b4c3d1.

--- hack/build-binaries.sh
+++ hack/build-binaries.sh
@@ -6,27 +6,13 @@ function get_latest_git_tag {
   git describe --tags | grep -Eo 'v[0-9]+\.[0-9]+\.[0-9]+'
 }
 
-VERSION="${1:-$(get_latest_git_tag)}"
-
-go fmt ./cmd/... ./pkg/... ./test/...
-go mod vendor
-go mod tidy
-
-# related to https://github.com/vmware-tanzu/carvel-imgpkg/pull/255
-# there doesn't appear to be a simple way to disable the defaultDockerConfigProvider
-# Having defaultDockerConfigProvider enabled by default results in the imgpkg auth ordering not working correctly
-# Specifically, the docker config.json is loaded before cli flags (and maybe even IaaS metadata services)
-git apply --ignore-space-change --ignore-whitespace ./hack/patch-k8s-pkg-credentialprovider.patch
-
-git diff --exit-code vendor/github.com/vdemeester || {
-  echo 'found changes in the project. when expected none. exiting'
-  exit 1
-}
+VERSION="${1:-`get_latest_git_tag`}"
 
 # makes builds reproducible
 export CGO_ENABLED=0
 LDFLAGS="-X github.com/vmware-tanzu/carvel-vendir/pkg/vendir/version.Version=$VERSION"
 repro_flags="-trimpath -mod=vendor"
+
 
 GOOS=darwin GOARCH=amd64 go build -ldflags="$LDFLAGS" $repro_flags -o vendir-darwin-amd64 ./cmd/vendir/...
 GOOS=darwin GOARCH=arm64 go build -ldflags="$LDFLAGS" $repro_flags -o vendir-darwin-arm64 ./cmd/vendir/...
