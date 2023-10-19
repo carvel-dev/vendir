82c3c9f4b9ef0d4e125e7b49057bca51b112be68
Fritz Duchardt <fritz@duchardt.net>
2023-10-19 08:15:55 +0200

    Introduced require test function in e2e test
    Signed-off-by: Fritz Duchardt <fritz@duchardt.net>

--- test/e2e/example_lazy_test.go
+++ test/e2e/example_lazy_test.go
@@ -4,6 +4,7 @@
 package e2e
 
 import (
+	"github.com/stretchr/testify/require"
 	ctlconf "github.com/vmware-tanzu/carvel-vendir/pkg/vendir/config"
 	"os"
 	"testing"
@@ -20,75 +21,36 @@ func TestExampleLazy(t *testing.T) {
 
 	// run lazy sync
 	_, err := vendir.RunWithOpts([]string{"sync", "-f=vendir-lazy.yml"}, RunOpts{Dir: path, Env: osEnv})
-	if err != nil {
-		t.Fatalf("Expected no err")
-	}
+	require.NoError(t, err)
 
+	// check that the lock file has config digest
 	lockConf, err := ctlconf.NewLockConfigFromFile(path + "/vendir.lock.yml")
-	if err != nil {
-		t.Fatalf("Expected no err: %s", err)
-	}
-
-	// find content digest
-	if len(lockConf.Directories[0].Contents[0].ConfigDigest) == 0 {
-		t.Fatalf("Expected Config Digest in Lock File")
-	}
+	require.NoError(t, err)
+	require.NotEmpty(t, lockConf.Directories[0].Contents[0].ConfigDigest, "Expected Config Digest in Lock File")
 
 	// remove some directory
 	err = os.RemoveAll(path + "/vendor/dir")
-	if err != nil {
-		t.Fatalf("Expected no err")
-	}
+	require.NoError(t, err)
 
 	// resync lazily, should not sync. Removed dir has not been reinstated
 	_, err = vendir.RunWithOpts([]string{"sync", "-f=vendir-lazy.yml"}, RunOpts{Dir: path, Env: osEnv})
-	if err != nil {
-		t.Fatalf("Expected no err")
-	}
-
-	_, err = os.Stat(path + "/vendor/dir")
-	if err == nil {
-		t.Fatalf("Expected err")
-	} else if !os.IsNotExist(err) {
-		t.Fatalf("Expected IsNotExist err")
-	}
+	require.NoError(t, err)
+	require.NoDirExists(t, path+"/vendor/dir")
 
 	// resync with lazy override, should not affect config digest
 	_, err = vendir.RunWithOpts([]string{"sync", "--lazy=false", "-f=vendir-lazy.yml"}, RunOpts{Dir: path, Env: osEnv})
-	if err != nil {
-		t.Fatalf("Expected no err")
-	}
-
-	stat, err := os.Stat(path + "/vendor/dir")
-	if err != nil {
-		t.Fatalf("Expected no err")
-	}
-	if !stat.IsDir() {
-		t.Fatalf("Expected Directory")
-	}
+	require.NoError(t, err)
+	require.DirExists(t, path+"/vendor/dir")
 
 	// content digest is kept during lazy sync override
 	lockConf, err = ctlconf.NewLockConfigFromFile(path + "/vendir.lock.yml")
-	if err != nil {
-		t.Fatalf("Expected no err")
-	}
-	if len(lockConf.Directories[0].Contents[0].ConfigDigest) == 0 {
-		t.Fatalf("Expected Config Digest in Lock File")
-	}
+	require.NoError(t, err)
+	require.NotEmpty(t, lockConf.Directories[0].Contents[0].ConfigDigest, "Expected Config Digest in Lock File")
 
 	// if synced without lazy flag in vendir.yml, no config digest should be kept in lock file
 	_, err = vendir.RunWithOpts([]string{"sync", "-f=vendir-nonlazy.yml"}, RunOpts{Dir: path, Env: osEnv})
-	if err != nil {
-		t.Fatalf("Expected no err")
-	}
-
+	require.NoError(t, err)
 	lockConf, err = ctlconf.NewLockConfigFromFile(path + "/vendir.lock.yml")
-	if err != nil {
-		t.Fatalf("Expected no err")
-	}
-
-	// Ensure no content digest set
-	if len(lockConf.Directories[0].Contents[0].ConfigDigest) != 0 {
-		t.Fatalf("Expected No Config Digest in Lock File")
-	}
+	require.NoError(t, err)
+	require.Empty(t, lockConf.Directories[0].Contents[0].ConfigDigest, "Expected No Config Digest in Lock File")
 }
