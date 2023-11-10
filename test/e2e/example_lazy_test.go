d317728fd366d98bd774285f66bc1ecf8e14a5fd
Fritz Duchardt <fritz@duchardt.net>
2023-11-10 09:00:09 +0100

    fix: e2e test
    Signed-off-by: Fritz Duchardt <fritz@duchardt.net>

--- test/e2e/example_lazy_test.go
+++ test/e2e/example_lazy_test.go
@@ -29,19 +29,19 @@ func TestExampleLazy(t *testing.T) {
 	require.NoError(t, err)
 	require.NotEmpty(t, lockConf.Directories[0].Contents[0].ConfigDigest, "Expected Config Digest in Lock File")
 
-	// remove some directory
-	err = os.RemoveAll(path + "/vendor/dir")
+	// remove file from synced dir
+	err = os.Remove(path + "/vendor/dir/some-file.txt")
 	require.NoError(t, err)
 
 	// resync lazily, should not sync. Removed dir has not been reinstated
 	_, err = vendir.RunWithOpts([]string{"sync", "-f=vendir-lazy.yml"}, RunOpts{Dir: path, Env: osEnv})
 	require.NoError(t, err)
-	require.NoDirExists(t, path+"/vendor/dir")
+	require.NoFileExists(t, path+"/vendor/dir/some-file.txt")
 
 	// resync with lazy override, should not affect config digest
 	_, err = vendir.RunWithOpts([]string{"sync", "--lazy=false", "-f=vendir-lazy.yml"}, RunOpts{Dir: path, Env: osEnv})
 	require.NoError(t, err)
-	require.DirExists(t, path+"/vendor/dir")
+	require.FileExists(t, path+"/vendor/dir/some-file.txt")
 
 	// content digest is kept during lazy sync override
 	lockConf, err = ctlconf.NewLockConfigFromFile(path + "/vendir.lock.yml")
