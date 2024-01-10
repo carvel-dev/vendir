5499ba36d10e7d4fd5e24dadd4a6c6d0a70a5180
German Lashevich <german.lashevich@gmail.com>
2024-01-10 22:31:13 +0100

    test: add e2e tests for overlapping paths
    Signed-off-by: German Lashevich <german.lashevich@gmail.com>

--- test/e2e/overlapping_dir_err_test.go
+++ test/e2e/overlapping_dir_err_test.go
@@ -6,19 +6,111 @@ package e2e
 import (
 	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestOverlappingDirErr(t *testing.T) {
+func TestOverlappingPathsErr(t *testing.T) {
+	tests := []struct {
+		name        string
+		description string
+		yaml        string
+		expectedErr string
+	}{
+		{
+			name:        "contents-paths",
+			description: "syncing config with overlapping contents paths",
+			yaml: `
+apiVersion: vendir.k14s.io/v1alpha1
+kind: Config
+directories:
+- path: vendor
+  contents:
+  - path: foo/bar/baz
+    inline:
+      paths:
+        test.txt: love
+  - path: foo
+    inline:
+      paths:
+        test.txt: peace
+`,
+			expectedErr: `vendir: Error: Parsing resource config '-':
+  Unmarshaling config:
+    Validating config:
+      Expected to not manage overlapping paths: 'vendor/foo/bar/baz' and 'vendor/foo'
+`,
+		},
+		{
+			name:        "directories-paths",
+			description: "syncing config with overlapping directories paths",
+			yaml: `
+apiVersion: vendir.k14s.io/v1alpha1
+kind: Config
+directories:
+- path: vendor
+  contents:
+  - path: foo/bar/baz
+    inline:
+      paths:
+        test.txt: love
+- path: vendor/foo
+  contents:
+  - path: bar
+    inline:
+      paths:
+        test.txt: peace
+`,
+			expectedErr: `vendir: Error: Parsing resource config '-':
+  Unmarshaling config:
+    Validating config:
+      Expected to not manage overlapping paths: 'vendor/foo' and 'vendor'
+`,
+		},
+		{
+			name:        "same-directories-paths",
+			description: "syncing config with the same directories paths",
+			yaml: `
+apiVersion: vendir.k14s.io/v1alpha1
+kind: Config
+directories:
+- path: vendor
+  contents:
+  - path: foo
+    inline:
+      paths:
+        test.txt: love
+- path: vendor
+  contents:
+  - path: bar
+    inline:
+      paths:
+        test.txt: peace
+`,
+			expectedErr: `vendir: Error: Parsing resource config '-':
+  Unmarshaling config:
+    Validating config:
+      Expected to not manage overlapping paths: 'vendor' and 'vendor'
+`,
+		},
+	}
+
 	env := BuildEnv(t)
 	vendir := Vendir{t, env.BinaryPath, Logger{}}
 
-	path := "../../examples/overlapping-dir"
-
-	_, err := vendir.RunWithOpts([]string{"sync"}, RunOpts{Dir: path, AllowError: true})
-	if err == nil {
-		t.Fatalf("Expected err")
-	}
-	if !strings.Contains(err.Error(), "Expected to not manage overlapping paths") {
-		t.Fatalf("Expected overlapping err: %s", err)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			_, err := vendir.RunWithOpts(
+				[]string{"sync", "-f", "-"},
+				RunOpts{
+					Dir:         t.TempDir(),
+					StdinReader: strings.NewReader(test.yaml),
+					AllowError:  true,
+				},
+			)
+			require.Error(t, err, "Expected to err while %s", test.description)
+			assert.ErrorContains(t, err, test.expectedErr)
+		})
 	}
 }
