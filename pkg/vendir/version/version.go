b6f315750ef7c5bf5eebee4f02fbb9f96be03c21
Joao Pereira <joaod@vmware.com>
2024-03-08 15:02:48 -0600

    Make version more flexible to allow vendir to be used as a library
    Signed-off-by: Joao Pereira <joaod@vmware.com>

--- pkg/vendir/version/version.go
+++ pkg/vendir/version/version.go
@@ -3,4 +3,39 @@
 
 package version
 
-var Version = "0.0.0+develop"
+import (
+	"runtime/debug"
+)
+
+var (
+	// Version can be set via:
+	// -ldflags="-X 'carvel.dev/vendir/pkg/vendir/version.Version=$TAG'"
+	defaultVersion = "0.0.0+develop"
+	Version        = ""
+	moduleName     = "carvel.dev/vendir"
+)
+
+func init() {
+	Version = version()
+}
+
+func version() string {
+	if Version != "" {
+		// Version was set via ldflags, just return it.
+		return Version
+	}
+
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return defaultVersion
+	}
+
+	// Anything else.
+	for _, dep := range info.Deps {
+		if dep.Path == moduleName {
+			return dep.Version
+		}
+	}
+
+	return defaultVersion
+}
